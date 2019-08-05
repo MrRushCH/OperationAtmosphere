05ecb783c7879585539eff1cca31a4d94d377ff9
Marc Rusch <mrusch@c18306-2.local>
2019-08-05 11:15:04 +0200

    Add addClass() instead of this.css()

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 //TODO use addClass()
-$(document).ready(function() {
+$(document).ready(function () {
   var stars = "";
-  console.log("ready!");
-  //function that makes cloud size, postiton and animation random
-  $(".a-cloud").each(function() {
+  var clouds = $(".a-cloud");
+  //function that generates random cloud size, postiton and animation duration/delay
+  clouds.each(function () {
+    //creates randomly generated position, delay, duration and size
     var randomPercentage = getRandomInt(0, 70) + "%";
-    var randomTime = getRandomInt(0, 10) + "s";
-    var randomSize = getRandomInt(0, 70) + "%";
-    // //TODO: mit Natlie anschauen
-    // $(this).css({
-    //   "animation-delay": randomTime,
-    //   "animation-duration": randomTime * 0.05,
-    //   top: randomPercentage,
-    //   zoom: randomSize
-    // });
-    var clouds = document.getElementsByClassName("cloud");
-    for (var i = 0; i < clouds.length; i++) {}
+    var randomDelay = getRandomInt(0, 20);
+    var randomTime = getRandomInt(10, 25);
+    var randomSize = getRandomInt(10, 70) + "%";
+
+    //sets randomly generated variables for each cloud
+    this.style.setProperty("--animation-delay", randomDelay + "s");
+    this.style.setProperty("--animation-duration", randomTime + "s");
+    this.style.setProperty("--top", randomPercentage);
+    this.style.setProperty("--zoom", randomSize);
+    $(this).addClass("a-cloud__animation-config");
   });
   //loop that makes star size and position random
   for (var i = 0; i <= 100; i++) {
